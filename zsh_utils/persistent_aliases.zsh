cf11f96b8bf4d4ffd04f885517a40f3771efdfc1
Daniel Shimon <daniel.shimon@axonius.com>
2024-05-29 11:35:51 +0300

    Update persist-aliases

--- zsh_utils/persistent_aliases.zsh
+++ zsh_utils/persistent_aliases.zsh
@@ -1,5 +1,4 @@
 _ALIASES_FILE="$HOME/.zsh_utils/paliases.zsh"
-_FUNCTIONS_FILE="$HOME/.zsh_utils/pfunctions.zsh"
 
 persist-alias() {
 	if ! which $1 >/dev/null; then
@@ -7,7 +6,7 @@ persist-alias() {
 		return 1
 	fi
 	sed -i '' "/^alias $1=/d" "$_ALIASES_FILE"
-	echo alias "$(alias | grep "^$1=")" >> "$_ALIASES_FILE"
+	echo "$(alias -L $1)" >> "$_ALIASES_FILE"
 }
 
 persist-function() {
@@ -19,6 +18,12 @@ sync-persistant() {
 }
 
 mkdir -p "$HOME/.zsh_utils"
-touch "$_ALIASES_FILE" "$_FUNCTIONS_FILE"
+touch "$_ALIASES_FILE"
 sync-persistant
+
+_comp-persist-alias() {
+	compadd $(alias -r | pcre2grep -o1 '^(\S+)=')
+}
+
+compdef _comp-persist-alias persist-alias
 
